6835fdb297400a59af16cede0fbe349e4d9aec31
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-12 04:28:17 +0000

    Update and rename Ytv.js to Bard.js

---
+++ plugins/Bard.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+let handler = async (m, { text, usedPrefix, command }) => {
+	if (!text) throw `Hay adakah yang bisa saya bantu??`;
+	await m.reply("Searching...");
+	let putra = await fetch(`https://aemt.me/bard?text=${text}`);
+	let hasil = await putra.json();
+	let txt = `${hasil.result}`.trim();
+	conn.sendMessage(m.chat, {
+		text: txt,
+		contextInfo: {
+			externalAdReply: {
+				title: `Bard Google Ai`,
+				body: wm,
+				thumbnailUrl: "https://telegra.ph/file/3d3535f2ea5f0f2034fff.jpg",
+				sourceUrl: "https:/bard.google.com",
+				mediaType: 1,
+				renderLargerThumbnail: true,
+			},
+		},
+	});
+};
+handler.help = ["bardgoogle *ᴛᴇxᴛ*"];
+handler.tags = [tai];
+handler.command = /^(bard)$/i;
+export default handler;
