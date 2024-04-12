83ea51d397a2e8b6df537da43f218e7c8e27d713
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-12 13:34:02 +0000

    Update Bard.js

--- plugins/Bard.js
+++ plugins/Bard.js
@@ -21,5 +21,5 @@ let handler = async (m, { text, usedPrefix, command }) => {
 };
 handler.help = ["bardgoogle *ᴛᴇxᴛ*"];
 handler.tags = ["ai"];
-handler.command = /^(bard)$/i;
+handler.command = /^(b)$/i;
 export default handler;
