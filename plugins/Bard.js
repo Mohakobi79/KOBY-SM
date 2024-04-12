b68fdd9632c843dc1037b78604f3ede539f862a3
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-12 13:37:38 +0000

    Update Bard.js

--- plugins/Bard.js
+++ plugins/Bard.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 let handler = async (m, { text, usedPrefix, command }) => {
 	if (!text) throw `*_✅مرحبا✌🏻، كيف يمكنني مساعدتك_*`;
-	await m.reply("'*_✋🏻نحن 😇 في خدمتكم✅،إنتظر(ي)..._*'");
+	await m.reply("`*_✋🏻نحن 😇 في خدمتكم✅،إنتظر(ي)..._*`");
 	let putra = await fetch(`https://aemt.me/bard?text=${text}`);
 	let hasil = await putra.json();
 	let txt = `${hasil.result}`.trim();
