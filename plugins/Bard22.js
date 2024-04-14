36b7c5cf9feb885021677be6956e311d252bad14
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-14 16:32:02 +0100

    Update and rename Bard2.js to Bard22.js

--- plugins/Bard22.js
+++ plugins/Bard22.js
@@ -3,8 +3,6 @@ import fetch from 'node-fetch';
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.ia_bard
 
   if (!text) {
