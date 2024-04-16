67f018380a3c6f0d2081005f638d6916050e0337
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-16 17:24:14 +0100

    Update Lyrics.js

--- plugins/Lyrics.js
+++ plugins/Lyrics.js
@@ -1,30 +1,22 @@
-import BuscarLetra from "../lib/lyricsgg.js";
+import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : "";
-  if (!teks) throw `️${lenguajeGB['smsAvisoMG']()}${mid.smsMalused}`;
-  try {
-    let res = await BuscarLetra(text);
-    let { titulo, artista, albulm, fecha, Generos, letra } = res;
-    let txt = `*𝙂𝙊𝙊𝙂𝙇𝙀 𝙇𝙔𝙍𝙄𝘾𝙎 🪴*\n\n`;
-    txt += ` *↬ ${mid.smsYT1}:* ${titulo}\n`;
-    txt += ` *↬ ${mid.smsYT6}:* ${artista}\n`;
-    txt += ` *↬ ${mid.smsYT7}:* ${albulm}\n`;
-    txt += ` *↬ ${mid.smsYT8}:* ${fecha}\n`;
-    txt += ` *↬ ${mid.smsYT8}:* ${Generos}\n\n`;
-    txt += `${letra}`;
-m.reply(txt);
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-handler.limit = false
-}};
-handler.command = handler.help = [
-  "google-lyrics",
-  "l",
-  "googlelyrics",
-];
-handler.limit = 1
-handler.register = true
-export default handler;
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+    if (!teks) throw `Use example ${usedPrefix}${command} friends`
+
+  await m.reply('Loading...')
+    const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
+    m.reply(`
+Lyrics *${result.title}*
+Author ${result.author}
+${result.lyrics}
+
+Url ${result.link}
+`.trim())
+}
+
+handler.help = ['lirik'].map(v => v + ' <Apa>')
+handler.tags = ['internet']
+handler.command = /^(li)$/i
+
+export default handler
