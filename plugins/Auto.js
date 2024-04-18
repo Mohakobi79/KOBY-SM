5f45e3e9510c7f1f3b9851b2278ef3bc6e4243f7
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-18 23:23:45 +0100

    Update Auto.js

--- plugins/Auto.js
+++ plugins/Auto.js
@@ -1,46 +1,38 @@
-const axios = require('axios');
-const fs = require('fs');
-const ytdl = require('ytdl-core');
 
-// دالة لتنزيل مقطع فيديو من YouTube
-async function downloadVideo(videoURL, filePath) {
-  // الحصول على معلومات الفيديو
-  const videoInfo = await ytdl.getInfo(videoURL);
+import fetch from 'node-fetch';
 
-  // تحديد أفضل دقة متاحة
-  const bestFormat = videoInfo.formats.find((format) => format.container === 'mp4' && format.progressive === true);
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+if (!text) throw 'You need to give the URL of Any Instagram video, post, reel, image';
+m.reply(wait);
 
-  // إنشاء مسار الملف
-  if (!fs.existsSync(filePath)) {
-    fs.mkdirSync(filePath, { recursive: true });
-  }
+if (!text.includes('instagram.com')) throw 'Invalid Instagram video URL';
 
-  const fileName = `${videoInfo.video_id}.mp4`;
-  const fileFullPath = `${filePath}/${fileName}`;
-
-  // تنزيل الفيديو
-  const response = await axios.get(bestFormat.url, { responseType: 'stream' });
-  const writer = fs.createWriteStream(fileFullPath);
-
-  response.data.pipe(writer);
-
-  // الانتظار حتى اكتمال التنزيل
-  await new Promise((resolve, reject) => {
-    writer.on('error', reject);
-    writer.on('finish', resolve);
-  });
-
-  console.log(`تم تنزيل الفيديو بنجاح: ${fileName}`);
+let res;
+try {
+res = await fetch(`${gurubot}/igdlv1?url=${text}`);
+} catch (error) {
+throw `An error occurred: ${error.message}`;
 }
 
-// دالة معالجة طلب تنزيل الفيديو
-async function handleDownloadRequest(videoURL, filePath) {
-  try {
-    await downloadVideo(videoURL, filePath);
-  } catch (error) {
-    console.error(`خطأ أثناء تنزيل الفيديو: ${error.message}`);
-  }
+let api_response = await res.json();
+if (!api_response || !api_response.data) {
+throw `No video or image found or Invalid response from API.`;
 }
 
-// تشغيل المعالج
-handleDownloadRequest('https://www.youtube.com/watch?v=VIDEO_ID', './videos');
+const mediaArray = api_response.data;
+for (const mediaData of mediaArray) {
+const mediaType = mediaData.type;
+const mediaURL = mediaData.url_download;
+let cap = `HERE IS THE ${mediaType.toUpperCase()} >,<`;
+
+if (['video', 'mp4'].includes(mediaType)) {
+conn.sendFile(m.chat, mediaURL, 'instagram.mp4', cap, m);
+} else if (['image', 'jpg', 'jpeg', 'png'].includes(mediaType)) {
+conn.sendFile(m.chat, mediaURL, 'instagram.jpg', cap, m);
+}
+}
+};
+
+handler.help = ['instagram'];
+handler.tags = ['downloader'];
+export default handler;
