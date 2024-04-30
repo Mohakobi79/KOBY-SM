874184d8a36de7ff4cfabf51716c391f74a3680d
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-04-30 17:30:56 +0100

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,2 +1,159 @@
-import axios from 'axios'; import instagramGetUrl from 'instagram-url-direct'; import {instagram} from '@xct007/frieren-scraper'; import {instagramdl} from '@bochilteam/scraper'; const handler = async (m, {conn, args, command, usedPrefix}) => { const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, "participant": "0@s.whatsapp.net" } if (!args[0]) throw `${lenguajeMY['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙎𝙐 𝙑𝙄𝘿𝙀𝙊 𝙊 𝙄𝙈𝘼𝙂𝙀𝙉\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.instagram.com/tv/Cd8U99IloVA/?igshid=YmMyMTA2M2Y=*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙔𝙊𝙐𝙍 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝙄𝙈𝘼𝙂𝙀\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://www.instagram.com/p/CCoI4DQBGVQ/?igshid=YmMyMTA2M2Y=*` const { key } = await conn.sendMessage(m.chat, {text: wait}, {quoted: fkontak}); 
- await delay(1000 * 2); await conn.sendMessage(m.chat, {text: waitt, edit: key}); await conn.sendMessage(m.chat, {text: waittt, edit: key}); await conn.sendMessage(m.chat, {text: waitttt, edit: key}); try { const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`; const responsel = await axios.get(apiUrll); const resultl = responsel.data; for (const item of resultl.message) { const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text(); let tXXxt = `✨ *ENLACE | URL:* ${shortUrRRl}\n\n${wm}`.trim() conn.sendFile(m.chat, item._url, null, tXXxt, m); await new Promise((resolve) => setTimeout(resolve, 10000)); }} catch { try { const datTa = await instagram.v1(args[0]); for (const urRRl of datTa) { const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text(); const tXXxt = `✨ *ENLACE | URL:* ${shortUrRRl}\n\n${wm}`.trim(); conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m); await new Promise((resolve) => setTimeout(resolve, 10000)); }} catch { try { const resultss = await instagramGetUrl(args[0]).url_list[0]; const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text(); const txt2 = `✨ *ENLACE | URL:* ${shortUrl2}\n\n${wm}`.trim(); await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m); } catch { try { const resultssss = await instagramdl(args[0]); const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text(); const txt4 = `✨ *ENLACE | URL:* ${shortUrl3}\n\n${wm}`.trim(); for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m); } catch { try { const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`); const json = await human.json(); const videoig = json.result; const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text(); const txt1 = `✨ *ENLACE | URL:* ${shortUrl1}\n\n${wm}`.trim(); await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m); } catch { console.log('Error en el intento 4, sin resultados') conn.sendMessage(m.chat, {text: `${lenguajeMY['smsAvisoFG']()} 𝗢𝗖𝗨𝗥𝗥𝗜𝗢 𝗨𝗡 𝙀𝙍𝙍𝙊𝙍 𝗜𝗡𝗘𝗦𝗣𝗘𝗥𝗔𝗗𝗢 𝙋𝙊𝙍𝙁𝘼𝙑𝙊𝙍 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝗥`, edit: key}); handler.limit = 0 }}}}}} handler.help = ['instagram '] handler.tags = ['downloader'] handler.command =/^(ko)$/i handler.limit = 2 handler.exp = 70 handler.register = false export default handler
+// Script coded by github.com/Xnuvers007
+import { instagramdl } from "@bochilteam/scraper";
+import { savefrom, snapsave } from "@bochilteam/scraper-sosmed";
+import fetch from "node-fetch";
+import ffmpeg from "fluent-ffmpeg";
+import fs from "fs/promises";
+
+var handler = async (m, { args, conn, usedPrefix, command }) => {
+  if (!args[0])
+    throw `Ex:\n${usedPrefix}${command} https://www.instagram.com/reel/C0EEgMNSSHw/?igshid=MzY1NDJmNzMyNQ==`;
+  const instagramUrlRegex = /^(https?:\/\/)?(www\.)?instagram\.com/i;
+  if (!instagramUrlRegex.test(args[0])) {
+    conn.reply(
+      m.chat,
+      `Harap masukkan link Instagram\ncontoh: ${usedPrefix}${command} https://www.instagram.com/reel/C0zv5N7ShOs/?utm_source=ig_web_copy_link`,
+      m
+    );
+    return;
+  }
+
+  let res;
+  try {
+    res = await snapsave(args[0]);
+    conn.reply(m.chat, "Sedang mengunduh video... pada server snapsave", m);
+  } catch (error1) {
+    try {
+      res = await instagramdl(args[0]);
+      conn.reply(
+        m.chat,
+        "Sedang mengunduh video... pada server instagramdl",
+        m
+      );
+    } catch (error2) {
+      try {
+        res = await savefrom(args[0]);
+        conn.reply(m.chat, "Sedang mengunduh video... pada server savefrom", m);
+      } catch (error3) {
+        console.log("error", error3);
+        conn.reply(m.chat, "Gagal mengunduh video", m);
+      }
+    }
+    conn.reply(m.chat, "error", m);
+  }
+
+
+  try {
+    let media = await res[0].url;
+    const sender = m.sender.split(`@`)[0];
+
+    // conn.reply(m.chat, 'Sedang mengunduh video... pada server 1', m);
+
+    if (!res) throw "Can't download the post";
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        video: { url: media },
+        caption: `ini kak videonya @${sender}`,
+        mentions: [m.sender],
+      },
+      m,
+    );
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        document: { url: media },
+        mimetype: "video/mp4",
+        fileName: `instagram.mp4`,
+        caption: `ini kak videonya @${sender} versi dokumen, agar jernih`,
+        mentions: [m.sender],
+      },
+      { quoted: m },
+    );
+
+    const mp3FileName = `suara.mp3`;
+    await convertVideoToMp3(media, mp3FileName);
+
+    await conn.sendFile(
+      m.chat,
+      mp3FileName,
+      mp3FileName,
+      `ini kak suaranya @${sender} versi MP3`,
+      m,
+    );
+
+    await fs.unlink(mp3FileName);
+  } catch (e) {
+    try {
+      let response = await fetch(
+        `https://tr.deployers.repl.co/instagramdl?url=${encodeURIComponent(
+          args[0],
+        )}`,
+      );
+      let data = await response.json();
+
+      if (data.image || data.video) {
+        const sender = m.sender.split(`@`)[0];
+
+        conn.reply(m.chat, "جاري التنزبل<", m);
+
+        await conn.sendMessage(
+          m.chat,
+          {
+            video: data.video,
+            caption: `ini kak videonya @${sender}`,
+            mentions: [m.sender],
+          },
+          m,
+        );
+
+        await conn.sendMessage(
+          m.chat,
+          {
+            document: { url: data.video },
+            mimetype: "video/mp4",
+            fileName: `dokumen.mp4`,
+            caption: `ini kak videonya @${sender} versi dokumen, agar jernih`,
+            mentions: [m.sender],
+          },
+          { quoted: m },
+        );
+
+        const mp3FileName = `suara.mp3`;
+        await convertVideoToMp3(data.video, mp3FileName);
+
+        await conn.sendFile(
+          m.chat,
+          mp3FileName,
+          mp3FileName,
+          `ini kak suaranya @${sender} versi MP3`,
+          m,
+        );
+
+        await fs.unlink(mp3FileName);
+      } else {
+        throw "Gagal mengunduh video";
+      }
+    } catch (error) {
+      conn.reply(m.chat, "Gagal mengunduh video", m);
+    }
+  }
+};
+
+async function convertVideoToMp3(videoUrl, outputFileName) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(videoUrl)
+      .toFormat("mp3")
+      .on("end", () => resolve())
+      .on("error", (err) => reject(err))
+      .save(outputFileName);
+  });
+}
+
+handler.help = ["instagram"];
+handler.tags = ["downloader"];
+handler.command = /^(ig(dl)?|instagram(dl)?)$/i;
+
+export default handler;
