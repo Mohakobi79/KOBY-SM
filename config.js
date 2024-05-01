223539692f91295178121e11d0d3dd6341a3349b
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-01 21:29:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,11 +26,7 @@ global.wm = '👻KOBY-BOT👻';
 global.titulowm = '👻KOBY-BOT👻';
 global.titulowm2 = `👻KOBY-BOT👻`
 global.igfg = '♤koba♤';
-global.wait = '《██▒▒▒▒▒▒▒▒▒▒▒》10%
- 《████▒▒▒▒▒▒▒▒》30%
- 《███████▒▒▒▒▒▒》50%
- 《██████████▒▒▒》70%  
-              *جارييي⏳  تحميل📥 الطلب*✅';
+global.wait = '*جارييي⏳  تحميل📥 الطلب* >';
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
