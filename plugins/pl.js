5ed1bc9522b5f9e56979878b443778bd2751131a
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-01 13:40:56 +0100

    Update pl.js

--- plugins/pl.js
+++ plugins/pl.js
@@ -1,26 +1,10 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 
-        if (!text) throw `> ✳️ online`
+        if (!text) throw `> ✅ online`
         let res = await yts(text)
         let vid = res.videos[0]
-        if (!vid) throw `✳️ Vídeo/Audio no encontrado`
-        let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
-        //const url = 'https://www.youtube.com/watch?v=' + videoId
-     //   m.react('🎧')
-        let play = `
-        ≡ *FG MUSIC*
-┌──────────────
-▢ 📌 *Título* : ${title}
-▢ 📆 *Publicado:* ${ago}
-▢ ⌚ *Duración:* ${timestamp}
-▢ 👀 *Vistas:* ${views}
-└──────────────`
-//await conn.sendButton(m.chat, play, thumbnail, [
- //   ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-//    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
- // ], m)
-}
+    
 handler.help = ['play']
 handler.tags = ['dl']
 handler.command = ['o']
