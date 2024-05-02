05e30fc2fd9898248bb1020f98c6b29e20a8aca4
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-02 08:02:13 +0100

    Update ply3.js

--- plugins/ply3.js
+++ plugins/ply3.js
@@ -13,7 +13,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   let vid = search.videos[0];
   if (!search) throw 'Video Not Found, Try Another Title';
   let { title, thumbnail, timestamp, views, ago, url } = vid;
-  let wm = 'R';
+  let wm = '☯️KOBY-BOT☯️';
 
   let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
   ⬡ TITLE: ${title}
