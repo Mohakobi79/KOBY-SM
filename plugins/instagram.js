1a46b0f8357ed1a6f689c4d446ecbda548117778
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-02 06:10:18 +0100

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,155 +1,42 @@
-// Script coded by github.com/Xnuvers007
-import { instagramdl } from "@bochilteam/scraper";
-import { savefrom, snapsave } from "@bochilteam/scraper-sosmed";
-import fetch from "node-fetch";
-import ffmpeg from "fluent-ffmpeg";
-import fs from "fs/promises";
+import fetch from 'node-fetch';
 
-var handler = async (m, { args, conn, usedPrefix, command }) => {
-  if (!args[0])
-    throw `Ex:\n${usedPrefix}${command} https://www.instagram.com/reel/C0EEgMNSSHw/?igshid=MzY1NDJmNzMyNQ==`;
-  const instagramUrlRegex = /^(https?:\/\/)?(www\.)?instagram\.com/i;
-  if (!instagramUrlRegex.test(args[0])) {
-    conn.reply(
-      m.chat,
-      `Harap masukkan link Instagram\ncontoh: ${usedPrefix}${command} https://www.instagram.com/reel/C0zv5N7ShOs/?utm_source=ig_web_copy_link`,
-      m
-    );
-    return;
-  }
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+  if (!text) throw `You need to give the URL of Any Instagram video, post, reel, image`;
+  m.reply(wait);
 
   let res;
   try {
-    res = await snapsave(args[0]);
-    
-  } catch (error1) {
-    try {
-      res = await instagramdl(args[0]);
-      
-    } catch (error2) {
-      try {
-        res = await savefrom(args[0]);
-        
-      } catch (error3) {
-        console.log("error", error3);
-       
-      }
-    }
-    conn.reply(m.chat, "error", m);
+    res = await fetch(`https://www.guruapi.tech/api/igdlv1?url=${text}`);
+  } catch (error) {
+    throw `An error occurred: ${error.message}`;
   }
 
+  let api_response = await res.json();
 
-  try {
-    let media = await res[0].url;
-    const sender = m.sender.split(`@`)[0];
+  if (!api_response || !api_response.data) {
+    throw `No video or image found or Invalid response from API.`;
+  }
 
-    // conn.reply(m.chat, 'Sedang mengunduh video... pada server 1', m);
+  const mediaArray = api_response.data;
 
-    if (!res) throw "Can't download the post";
+  for (const mediaData of mediaArray) {
+    const mediaType = mediaData.type;
+    const mediaURL = mediaData.url_download;
 
-    await conn.sendMessage(
-      m.chat,
-      {
-        video: { url: media },
-        caption: `ini kak videonya @${sender}`,
-        mentions: [m.sender],
-      },
-      m,
-    );
+    let cap = `< TÉLÉCHARGÉ✅${mediaType.toUpperCase()}`;
 
-    ///await conn.sendMessage(
-   //   m.chat,
-  //    {
-  //      document: { url: media },
-    //    mimetype: "video/mp4",
-      //  fileName: `instagram.mp4`,
-     //   caption: `ini kak videonya @${sender} versi dokumen, agar jernih`,
-     //   mentions: [m.sender],
-   //   },
-   //   { quoted: m },
- //   );
+    if (mediaType === 'video') {
 
- //   const mp3FileName = `suara.mp3`;
-  //  await convertVideoToMp3(media, mp3FileName);
+      conn.sendFile(m.chat, mediaURL, 'instagram.mp4', cap, m, null);
+    } else if (mediaType === 'image') {
 
- //   await conn.sendFile(
-  //    m.chat,
- //     mp3FileName,
-    //  mp3FileName,
-     // `ini kak suaranya @${sender} versi MP3`,
-//      m,
-   // );
-
-   // await fs.unlink(mp3FileName);
-//  } catch (e) {
- //   try {
-  //    let response = await fetch(
- ////       `https://tr.deployers.repl.co/instagramdl?url=${encodeURIComponent(
-    //      args[0],
-      //  )}`,
-      );
-  //    let data = await response.json();
-
-  //    if (data.image || data.video) {
-    //    const sender = m.sender.split(`@`)[0];
-
-    //    conn.reply(m.chat, "جاري التنزبل<", m);
-
-        await conn.sendMessage(
-          m.chat,
-          {
-            video: data.video,
-            caption: `ini kak videonya @${sender}`,
-            mentions: [m.sender],
-          },
-          m,
-        );
-
-     //   await conn.sendMessage(
-       //   m.chat,
-          {
-      //      document: { url: data.video },
-         //   mimetype: "video/mp4",
-         //   fileName: `dokumen.mp4`,
-       //     caption: `ini kak videonya @${sender} versi dokumen, agar jernih`,
-         //   mentions: [m.sender],
-      //    },
-        //  { quoted: m },
-     //   );
-
-  //      const mp3FileName = `suara.mp3`;
-    //    await convertVideoToMp3(data.video, mp3FileName);
-
-   //     await conn.sendFile(
-   //       m.chat,
-   //       mp3FileName,
-     //     mp3FileName,
-     //     `ini kak suaranya @${sender} versi MP3`,
-          m,
-        );
-
-        await fs.unlink(mp3FileName);
-      } else {
-        throw "Gagal mengunduh video";
-      }
-    } catch (error) {
-      conn.reply(m.chat, "Gagal mengunduh video", m);
+      conn.sendFile(m.chat, mediaURL, 'instagram.jpg', cap, m, null);
     }
   }
 };
 
-async function convertVideoToMp3(videoUrl, outputFileName) {
-  return new Promise((resolve, reject) => {
-    ffmpeg(videoUrl)
-      .toFormat("mp3")
-      .on("end", () => resolve())
-      .on("error", (err) => reject(err))
-      .save(outputFileName);
-  });
-}
-
-handler.help = ["instagram"];
-handler.tags = ["downloader"];
-handler.command = /^(ig(dl)?|instagram(dl)?)$/i;
+handler.help = ['instagram'];
+handler.tags = ['downloader'];
+handler.command = /^(instagram|ic|ig|insta)$/i;
 
 export default handler;
