042dbc399d0d4bda4a6f5045ab499b589d898a6c
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-14 17:17:48 +0100

    Update ai-img.js

--- plugins/ai-img.js
+++ plugins/ai-img.js
@@ -1,12 +1,21 @@
-import { googleImage } from '@bochilteam/scraper'
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-const paroleproibite = ['se men', 'hen tai', 'se xo', 'te tas', 'cu lo', 'c ulo', 'cul o', 'ntr', 'rule34', 'rule', 'caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito', 'pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra','rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'xxx', 'nuda', 'nude', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta']
-if (paroleproibite.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '⚠️ 𝐍𝐨𝐧 𝐩𝐨𝐬𝐬𝐨 𝐢𝐧𝐯𝐢𝐚𝐫𝐞 𝐪𝐮𝐞𝐬𝐭𝐨 𝐜𝐨𝐧𝐭𝐞𝐧𝐮𝐭𝐨', m,)
-if (!text) return conn.reply(m.chat, `> ⓘ 𝐔𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix + command} gatto`, m,)
-const res = await googleImage(text)
-let image = res.getRandom()
-let link = image
-conn.sendFile(m.chat, link, 'errore.jpg', `🔍 𝐈𝐦𝐦𝐚𝐠𝐢𝐧𝐞: ${text}`, m)
-}
-handler.command = /^(immagine|imgai|immagini)$/i
+import fetch from 'node-fetch'
+var handler = async (m, { text,  usedPrefix, command }) => {
+if (!text) throw `🍃 *Ingres Una Petición*\n- Ejemplo: !bard conoces a yotsuba?`
+try {
+await m.reply('🌺 *C A R G A N D O*\n- 🍃 Cargando Información')
+conn.sendPresenceUpdate('composing', m.chat);
+var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
+} catch (e) {
+await conn.reply(m.chat, `*🍃 Ocurrió Un Error Inesperado*`, m)
+console.log(`🍁 *Ocurrió Un Error*`)
+console.log(e)
+};
+handler.command = ['ge']
+handler.help = ['ge']
+handler.tags = ['ai']
+
+handler.premium = false
+
 export default handler
