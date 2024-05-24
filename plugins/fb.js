8ff1d2b19ea17545c2d7e6581429ad4008e5b27a
Kobasimo12 <132276378+Kobasimo12@users.noreply.github.com>
2024-05-24 19:45:33 +0100

    Update plugins

---
+++ plugins/fb.js
@@ -0,0 +1,11 @@
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `> خاص بالتنزيل بالفيسبوك ،مثال.fb وضع الرابط 🔗`
+const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, 
+`> Téléchargé📥 avec ↔️ succès ✅`, m)
+}
+handler.help = ['fb0'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+export default handler
