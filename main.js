4e83757734c798b1d88e1997d9eae324dfe6f5f3
Mohakobi79 <mohakobi79@gmail.com>
2024-12-16 22:45:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -531,7 +531,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn?.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `🌼 Uptime:♻️ ${uptime}🌤`;
   await conn?.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
