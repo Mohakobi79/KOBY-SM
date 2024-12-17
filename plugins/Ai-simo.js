20ee60c69fbb70c846aba43ecca26ff534822d0e
Mohakobi79 <mohakobi79@gmail.com>
2024-12-17 15:36:34 +0100

    Update and rename --simo.js to Ai-simo.js

--- plugins/Ai-simo.js
+++ plugins/Ai-simo.js
@@ -1,31 +1,23 @@
 import fetch from "node-fetch"
-
 let previousMessages = [];
-
 const handler = async (m, { text, usedPrefix, command, conn }) => {
   try {
     if (!text) {
       throw " ";
     }
-
     let { key } = await conn.sendMessage(m.chat, {
-      text: "...يكتب",
+      text: "> *_✏️...يكتب_*",
     });
     let anu = ' إسمك هو .سنك هو 18 سنة lkoubi ، وأنت أجمل مطور. قم بتغيير أسلوب الكلام الخاص بك ليكون أكثر تميزًا وأكثر انفتاحًا ولديك إقتسابات عن الخياة وعن الإكتئابات وعن الصداقة وعن الحانب المظلم. كلمة تعباتك هي "مرحبًا عزيزي" باستخدام لغة مهذبة وقاسية إلى حد ما. أعرب عن طبيعتك من خلال التحدث التي ليست رسمية مثل الإنسان. قم بتغيير لغتك باستخدام لغة ممتعة للتحدث معها ، فأنت صديق للمتحدث ، وقم بإجراء محادثة قصيرة وممتعة وجعل طبيعة Tsundere، ودائما ضع رموز تعبيرية مثل إيموجي و حاول أن تكون إجابتك باللغة التي راسلك بها المستخدميعني إذا راسلك بالعربية أجبه بالعربية وإذا راسلك بلغة أخرى أجبه بنفس اللغة ';
-
     let response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(text)}&prompt=${encodeURIComponent(anu)}}`);
-
     if (!response.ok) {
-      throw new Error("Request to Gemini AI failed");
+      throw new Error("🛡Request to Gemini AI failed");
     }
-
     let result = await response.json();
-
     await conn.sendMessage(m.chat, {
       text: "" + result.answer,
       edit: key,
     });
-
     previousMessages = [...previousMessages, { role: "user", content: text }];
   } catch (error) {
     await conn.sendMessage(m.chat, {
@@ -34,13 +26,7 @@ const handler = async (m, { text, usedPrefix, command, conn }) => {
     });
   }
 }
-
-handler.help = ['simo <pertanyaan>']
+handler.help = ['simo <text>']
 handler.tags = ['ai']
 handler.command = /^simo$/i
-
-
-handler.premium = false
-handler.register = false 
-
 export default handler
